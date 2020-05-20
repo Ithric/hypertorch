53499678e9d9354acc3e5b20058a9752fd69f6b9
Ronny Kaste <rkaste@gmail.com>
2020-05-20 14:46:29 +0200

    bump

--- hypertorch/searchspaceprimitives.py
+++ hypertorch/searchspaceprimitives.py
@@ -3,10 +3,10 @@ class SearchSpacePrimitive(object):
     pass
 
 class IntSpace(SearchSpacePrimitive):
-    def __init__(self, from_int, to_int):
+    def __init__(self, from_int, to_int, default=None):
         self.from_int = from_int
         self.to_int = to_int
-        self.default = None
+        self.default = default
 
     def __str__(self):
         return "IntSpace(from_int={}, to_int={})".format(self.from_int, self.to_int)
@@ -15,7 +15,7 @@ class FloatSpace(SearchSpacePrimitive):
     def __init__(self, from_float, to_float, default=None):
         self.from_float = from_float
         self.to_float = to_float
-        self.default = None
+        self.default = default
 
     def __str__(self):
         return "FloatSpace(from_float={}, to_float={})".format(self.from_float, self.to_float)
@@ -28,9 +28,9 @@ class NoSpace(SearchSpacePrimitive):
         return "Exactly({})".format(self.exact_value)
 
 class OneOfSet(SearchSpacePrimitive):
-    def __init__(self, set_values):
+    def __init__(self, set_values, default=None):
         self.set_values = [k for k in set_values]
-        self.default = None
+        self.default = default
     
     def __str__(self):
         return "OneOfSet({})".format(self.set_values)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="hypertorch",
-    version="0.0.13",
+    version="0.0.14",
     packages=["hypertorch"],
     license="MIT License",
     depends=["torch","numpy","toolz"],
